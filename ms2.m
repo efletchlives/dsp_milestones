b65bd4f1c45ae431803c7da3469b3b4f140bc36a
Evan Fletcher <ehf21@pitt.edu>
2025-09-30 19:47:38 +0000

    yo

---
+++ ms2.m
@@ -0,0 +1,17 @@
+wav_file = 
+wav_file = 
+wav_file = 
+wav_file = 
+wav_file = 
+wav_file = 
+
+audio_file = audioread(wav_file)
+
+% time-domain algorithms (autocorrelation)
+
+[acf,lags] = autocorr(audio_file)
+
+
+
+
+% frequency-domain algorithms
